819ff05360faf9c4f6a13ffe237159db1a5d6201
Martin Page <t2@funler.com>
2016-07-12 19:49:39 -0300

    Load any exercise

--- src/demo-perseus.js
+++ src/demo-perseus.js
@@ -51,7 +51,7 @@ const defaultQuestion = {
 
 Perseus.init({skipMathJax: false, loadExtraWidgets: true}).then(function() {
     $(Exercises).bind("readyForNextProblem", function() {
-      var question = window.khanExerciseLoader.currentExercise;
+      var question = JSON.parse(window.khanExerciseLoader.currentExercise.data.json);
       var problemNum = Math.floor(Math.random() * 100);
       var questinInfo = [RendererDemo, {question, problemNum}];
       DemoComponent = ReactDOM.render(
