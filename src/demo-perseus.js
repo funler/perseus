5858d9acdad5575f4d186c8a2db7598ecae6c3ee
Martin Page <t2@funler.com>
2016-07-28 16:32:58 -0300

    Removed unused code

--- src/demo-perseus.js
+++ src/demo-perseus.js
@@ -4,7 +4,6 @@
  * This file initializes the Khan globals and mounts Demo pages
  * to demonstrate and develop the Perseus application
  */
-
 require('./perseus-env.js');
 
 window.Khan = {
@@ -20,34 +19,6 @@ const ReactDOM = window.ReactDOM = React.__internalReactDOM;
 const EditorDemo = require('./editor-demo.jsx');
 const RendererDemo = require('./renderer-demo.jsx');
 const ArticleDemo = require('./article-demo.jsx');
-
-const defaultQuestion = {
-    "question": {
-        "content": "",
-        "images": {},
-        "widgets": {},
-    },
-    "answerArea": {
-        "calculator": false,
-    },
-    "itemDataVersion": {
-        "major": 0,
-        "minor": 1,
-    },
-    "hints": [],
-};
-
-// // const query = Perseus.Util.parseQueryString(window.location.hash.substring(1));
-// const question = query.content ? JSON.parse(query.content) : defaultQuestion;
-// const problemNum = Math.floor(Math.random() * 100);
-//
-// // React router v20XX
-// const path = 'renderer';//window.location.search.substring(1);
-// const routes = { // The value is spread across a React.createElement call
-//     'renderer': [RendererDemo, {question, problemNum}],
-//     'article': [ArticleDemo, {content: question}],
-//     '': [EditorDemo, {question, problemNum}],
-// };
 
 Perseus.init({skipMathJax: false, loadExtraWidgets: true}).then(function() {
     $(Exercises).bind("gotoNextProblem", function() {
