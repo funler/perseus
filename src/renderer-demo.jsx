39a0cfd5095bf7ba8e1bbbc041429e66c5e68940
Martin Page <t2@funler.com>
2016-08-17 21:11:03 -0300

    Calculator improvments

--- src/renderer-demo.jsx
+++ src/renderer-demo.jsx
@@ -230,12 +230,14 @@ const RendererDemo = React.createClass({
                                 </div>
 
                                 <div className="extra-button-box">
-                                  <button type="button" id="calc-show-button" onClick={$('#calculator').toggle} className="show_calculator">Show calculator</button>
+                                  <button type="button" id="calc-show-button" onClick={(function() {$('#calculator').show()})} className="show_calculator">calculator</button>
                                 </div>
 
                                 <div className="info-box" id="calculator" style={{display:"none"}}>
-                                  <span className="info-box-header">Calculator</span>
-                                  <button type="button" id="calc-hide-button" onClick={$('#calculator').toggle} className="hide_calculator">Hide</button>
+                                  <div className="calculator-header">
+                                      <span className="info-box-header inline">Calculator</span>
+                                      <button type="button" id="calc-hide-button" onClick={(function() {$('#calculator').hide()})} className="hide-calculator btn btn-default glyphicon glyphicon-remove"></button>
+                                  </div>
                                   <div className="calculator">
                                       <div className="history">
                                           <div id="calc-output">
