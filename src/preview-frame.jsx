cffad75df5a49847d02335d87c2974a5a9401d82
Evgeny Svirsky <evgeny@funler.com>
2017-11-24 18:12:14 +0300

    Ignore isMobile parameter for right panel.

--- src/preview-frame.jsx
+++ src/preview-frame.jsx
@@ -127,7 +127,7 @@ const PreviewFrame = React.createClass({
             const perseusClass =
                 "framework-perseus fonts-loaded " +
                 (isExercise ? "bibliotron-exercise " : "bibliotron-article ") +
-                (this.props.isMobile ? "perseus-mobile" : "");
+                (true ? "perseus-mobile" : "");
 
             const linterContext = this.state.data.linterContext;
 
